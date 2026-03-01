c0ed3a4acaf9a2c426d4ba63b58eab0b75ba7a30
SZanko <szanko_dev@proton.me>
2026-03-01 16:01:41 +0000

    Add search functionality to searchbar
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- desktop/src/lib.rs
+++ desktop/src/lib.rs
@@ -1,11 +1,11 @@
 mod config;
 
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex, OnceLock};
-use log::log;
-use slint::{Color, Image, ModelRc, SharedString, VecModel};
 use carris_api::api::CarrisClient;
 use carris_api::types::{Arrival, CarrisAPI, Stop};
+use log::log;
+use slint::{Color, Image, ModelRc, SharedString, VecModel, Weak};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex, OnceLock};
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
@@ -26,13 +26,15 @@ impl From<Arrival> for BusArrival {
         BusArrival {
             number: arrival.line_id as i32,
 
-            arrival_time: arrival.scheduled_arrival.unwrap_or_else(|| "--:--".into()).into(),
+            arrival_time: arrival
+                .scheduled_arrival
+                .unwrap_or_else(|| "--:--".into())
+                .into(),
 
             direction: arrival.headsign.into(),
         }
     }
 }
-
 
 fn stops_to_models(stops: Vec<Stop>) -> (ModelRc<ListItem>, ModelRc<SharedString>) {
     let mut items = Vec::with_capacity(stops.len());
@@ -42,7 +44,7 @@ fn stops_to_models(stops: Vec<Stop>) -> (ModelRc<ListItem>, ModelRc<SharedString
         ids.push(SharedString::from(s.id.clone()));
 
         items.push(ListItem {
-            text: s.long_name.into(),          // what user sees
+            text: s.long_name.into(),     // what user sees
             supporting_text: s.id.into(), // optional (e.g. municipality)
             avatar_icon: Image::default(),
             avatar_text: SharedString::new(),
@@ -52,41 +54,35 @@ fn stops_to_models(stops: Vec<Stop>) -> (ModelRc<ListItem>, ModelRc<SharedString
         });
     }
 
-    (ModelRc::new(VecModel::from(items)), ModelRc::new(VecModel::from(ids)))
+    (
+        ModelRc::new(VecModel::from(items)),
+        ModelRc::new(VecModel::from(ids)),
+    )
 }
-
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
+    env_logger::init();
     let ui = ui();
 
-    let lookup: Arc<Mutex<HashMap<String, String>>> =
-        Arc::new(Mutex::new(HashMap::new()));
+    let lookup: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
     let lookup_for_task = lookup.clone();
+    let lookup_for_stop = lookup.clone();
     let ui_handle_stops = ui.clone_strong();
 
-    fill_searchbar_with_options(lookup_for_task, &ui);
+    fill_searchbar_with_options(&ui, lookup_for_task);
 
-    update_selected_bus_stop(&ui, lookup);
-
-
+    update_selected_bus_stop(&ui, &lookup);
 
     ui.on_searchbar_bus_station_clicked(move |index| {
         log::info!("Search for {index}");
     });
 
-    let ui_for_task = ui.clone_strong();
+    let ui_searchbar_weak = ui.as_weak();
+    let lookup_searchbar_cb = lookup.clone();
 
     ui.on_filter_searchbar_options(move |text: SharedString| {
-        let ui_for_task = ui_for_task.clone_strong();
-        slint::spawn_local(async move {
-            let text = text.to_string();
-            log::info!("(async) got text = {text}");
-
-
-            //ui_for_task.set_bus_stations(filter_search_results(&*text));
-        })
-            .unwrap();
+        spawn_filter_search(ui_searchbar_weak.clone(), lookup_searchbar_cb.clone(), text);
     });
 
 
@@ -98,7 +94,75 @@ pub fn main() {
     ui.run().unwrap();
 }
 
-fn update_selected_bus_stop(ui: &MainWindow, lookup: Arc<Mutex<HashMap<String, String>>>) {
+fn spawn_filter_search(
+    ui_weak: Weak<MainWindow>,
+    lookup: Arc<Mutex<HashMap<String, String>>>,
+    text: SharedString) {
+    slint::spawn_local(async_compat::Compat::new(async move {
+        if text.is_empty() {
+            return;
+        }
+
+        let query = text.to_string();
+        log::info!("(async) got text = {query}");
+
+        let results = filter_search_string(&query, &lookup, 25);
+        let mut filter_result: Vec<ListItem> = Vec::with_capacity(results.len());
+
+        let map = lookup.lock().unwrap();
+
+        for result in results {
+            let key = result.clone();
+
+            let supporting = map
+                .get(key.as_str())
+                .cloned()
+                .unwrap_or_default();
+
+            filter_result.push(ListItem {
+                text: result.into(),
+                supporting_text: supporting.into(),
+                avatar_icon: Image::default(),
+                avatar_text: SharedString::new(),
+                avatar_background: Color::from_argb_u8(0, 0, 0, 0),
+                avatar_foreground: Color::from_argb_u8(0, 0, 0, 0),
+                action_button_icon: Image::default(),
+            })
+        }
+        drop(map);
+
+        if let Some(ui) = ui_weak.upgrade() {
+            ui.set_bus_stations(ModelRc::new(VecModel::from(filter_result)));
+        }
+    })).expect("Cannot filter search options in searchbar");
+}
+
+fn filter_search_string(
+    query: &str,
+    lookup: &Arc<Mutex<HashMap<String, String>>>,
+    limit: usize,
+) -> Vec<SharedString> {
+    let lquery = query.trim().to_lowercase();
+    if lquery.is_empty() {
+        return vec![];
+    }
+
+    let keys: Vec<String> = {
+        let map = lookup.lock().unwrap();
+        map.keys().cloned().collect()
+    };
+
+    let results = keys
+        .into_iter()
+        .filter(|name| name.to_lowercase().contains(&lquery))
+        //.take(limit)
+        .map(|name| SharedString::from(name))
+        .collect();
+
+    return results;
+}
+
+fn update_selected_bus_stop(ui: &MainWindow, lookup: &Arc<Mutex<HashMap<String, String>>>) {
     let ui_for_cb = ui.clone_strong();
     let lookup_for_cb = lookup.clone();
     ui.on_bus_station_selected(move |search_text: SharedString| {
@@ -125,7 +189,8 @@ fn update_selected_bus_stop(ui: &MainWindow, lookup: Arc<Mutex<HashMap<String, S
                 }
                 Err(e) => log::error!("Failed to load arrivals for {stop_id}: {e}"),
             }
-        })).unwrap();
+        }))
+            .unwrap();
     });
 }
 
@@ -142,10 +207,14 @@ fn set_bus_arrivals_for_station(ui: &MainWindow) {
         log::info!("Length of the content is: {}", bus_arrivals.len());
         let model = ModelRc::new(VecModel::from(bus_arrivals));
         ui_handle_busses.set_next_busses(model);
-    })).expect("Cannot get Bus Data");
+    }))
+        .expect("Cannot get Bus Data");
 }
 
-fn fill_searchbar_with_options(lookup_for_task: Arc<Mutex<HashMap<String, String>>>, ui: &MainWindow) {
+fn fill_searchbar_with_options(
+    ui: &MainWindow,
+    lookup_for_stop: Arc<Mutex<HashMap<String, String>>>,
+) {
     let ui_handle_stops = ui.clone_strong();
 
     slint::spawn_local(async_compat::Compat::new(async move {
@@ -160,14 +229,15 @@ fn fill_searchbar_with_options(lookup_for_task: Arc<Mutex<HashMap<String, String
                 for s in lookup_stops {
                     map.insert(s.long_name, s.id);
                 }
-                *lookup_for_task.lock().unwrap() = map;
+                *lookup_for_stop.lock().unwrap() = map;
             }
             Err(e) => {
                 log::error!("Failed to load stops: {e}");
                 ui_handle_stops.set_busstation_label("Cannot load all stops".into())
             }
         }
-    })).unwrap();
+    }))
+        .unwrap();
 }
 //fn filter_search_results(input: &str, existing_bus_stops_original: Vec<>, ) -> ModelRc<ListItem> {
 //
